07fd146a6001542e24438f1f34ae2765c6773851
elBerdLoco <sandotomsando@gmail.com>
2022-05-03 11:00:35 -0600

    Base project done, need buzzer

--- Project_3/draw_shapes.c
+++ Project_3/draw_shapes.c
@@ -15,62 +15,78 @@ update_shape(void)
   static unsigned char row = screenHeight / 2, col = screenWidth / 2;
   static char blue = 31, green = 0, red = 31;
   static unsigned char step = 0;
-  static unsigned char circle1 = 5;
-  static unsigned char circle2 = 0;
+  static unsigned char nameCounter = 0;
   
-  if (switches & SW4) return;
-  if (step <= 80) {
+  
+  //if (switches & SW4) return;
+  if (step <= 60) {
     // a color in this BGR encoding is BBBB BGGG GGGR RRRR
     unsigned int color = (blue << 11) | (green << 5) | red;
     
-    if(step <= 20){
-      fillRectangle(15, 40+step, 10, 10, color);  //middle of T
-      fillRectangle(40, 40+step, 10, 10, color);  //left side of o
-      fillRectangle(60, 40+step, 10, 10, color);  //right side of o
-      fillRectangle(5+step, 40, 10, 10, color);   //top of T 
-      fillRectangle(40+step, 60, 10, 10, color);  //top of o 
-      fillRectangle(40+step, 40, 10, 10, color);  //bottom of o
-      fillRectangle(75, 40+step, 10, 10, color);  //left m column
-      fillRectangle(95, 40+step, 10, 10, color); //midle m column
-      fillRectangle(115, 40+step, 10, 10, color); //right m column
-      fillRectangle(75+step, 40, 10, 10, color);  //top left row of m
-      fillRectangle(95+step, 40, 10, 10, color); //top right row of m
-    }
-    if(step <= 50){
-      fillRectangle(10 + step, 80, 5 + step, 5, color);
+    fillRectangle(15, 40+nameCounter, 10, 10, color);  //middle of T
+    fillRectangle(40, 40+nameCounter, 10, 10, color);  //left side of o
+    fillRectangle(60, 40+nameCounter, 10, 10, color);  //right side of o
+    fillRectangle(5+nameCounter, 40, 10, 10, color);   //top of T 
+    fillRectangle(40+nameCounter, 60, 10, 10, color);  //top of o 
+    fillRectangle(40+nameCounter, 40, 10, 10, color);  //bottom of o
+    fillRectangle(75, 40+nameCounter, 10, 10, color);  //left m column
+    fillRectangle(95, 40+nameCounter, 10, 10, color); //midle m column
+    fillRectangle(115, 40+nameCounter, 10, 10, color); //right m column
+    fillRectangle(75+nameCounter, 40, 10, 10, color);  //top left row of m
+    fillRectangle(95+nameCounter, 40, 10, 10, color); //top right row of m
+    
+    fillRectangle(10 + step, 80, 5 + step, 5, color); // line under name
+    nameCounter++;
+    if (nameCounter >= 20){
+      nameCounter = 0;
     }
     step++;
     if (switch1_down) {         //CAP A
-      green = (green + 1) % 69;
+      green = (green + 3) % 37;
       fillRectangle(0 + (step)*(2), 90, 40 + (step)*(2), 20 + step, color);
       if (switch2_down) {            //CAP B
+	 step = 0;
 	clearScreen(COLOR_YELLOW);
-	drawString5x7(20, 40 + step, "This is YELLOW", COLOR_GREEN, COLOR_YELLOW);
+	drawString5x7(20, 10, "This is YELLOW", COLOR_GREEN, COLOR_YELLOW);
 	if (switch3_down){           //CAP C
+	   step = 0;
 	  clearScreen(COLOR_RED);
-	  drawString5x7(20, 40 + step, "This is RED", COLOR_GREEN, COLOR_RED);
+	  drawString5x7(20, 10, "This is RED", COLOR_GREEN, COLOR_RED);
 	}
       }
       else if (switch3_down) {       //CAP B
+	 step = 0;
 	clearScreen(COLOR_GREEN);
-	drawString5x7(20, 40 + step, "This is GREEN", COLOR_RED, COLOR_GREEN);
+	drawString5x7(20, 10, "This is GREEN", COLOR_RED, COLOR_GREEN);
       }
     }
     else if (switch2_down) {
-      blue = (blue + 2) % 34;
+      blue = (blue + 2) % 27;
       if(step <= 60){
-	fillRectangle(0 + step, 150 - step, 1 + step , 1, color);
+	fillRectangle(step, 150 - step, 1 + step , 1, color);
       }
       if(switch3_down) {
 	clearScreen(COLOR_BLUE);
-	drawString5x7(20, 40 + step, "This is BLUE", COLOR_RED, COLOR_BLUE);
+	 step = 0;
+	drawString5x7(20, 10, "This is BLUE", COLOR_RED, COLOR_BLUE);
       }
     }
-    else if(switch3_down) {
-      red = (red - 3) % 34;
+    else if(switch3_down) { 
+      red = (red - 3) % 17;
       if(step <= 60){
 	fillRectangle(60, 150 - step, 1 + step, 1, color);
       } 
+    }
+    else if(switch4_down) {
+      if(step == 0 ){
+	fillRectangle(9 + nameCounter, 9, 60, 20, COLOR_BLACK);
+	fillRectangle(60, 9, 60, 20, COLOR_BLACK);
+      }
+      if(step >= 20){
+	fillRectangle(60 + nameCounter, 9, 1, 20, COLOR_BLACK);
+      }
+      fillRectangle(5 + nameCounter, 5, 60, 20, COLOR_BLACK);
+      drawString5x7(10 + nameCounter, 10, "SUCCESS!!!", COLOR_GREEN, COLOR_BLACK);
     }
   }else {
     step = 0;

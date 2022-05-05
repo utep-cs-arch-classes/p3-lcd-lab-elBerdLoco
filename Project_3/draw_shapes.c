499f35caa00dec1e7d6c376aaea19e25941cbb65
elBerdLoco <sandotomsando@gmail.com>
2022-05-04 19:47:30 -0600

    Version 2.5

--- Project_3/draw_shapes.c
+++ Project_3/draw_shapes.c
@@ -16,93 +16,86 @@ update_shape(void)
   static char blue = 31, green = 0, red = 31;
   static unsigned char step = 0;
   static unsigned char nameCounter = 0;
-  
+  static unsigned char songCounter = 0;
+  static unsigned char bounce2 = 0;
+  static unsigned char currentX = 0;
+  static unsigned char currentY = 0;
+  static unsigned char currentWidth = 0;
+  static unsigned char currentHeight = 0;
+  static unsigned int background = 0; 
   
   //if (switches & SW4) return;
   if (step <= 60) {
     buzzer_init();
-    buzzer_set_period(1000);
+    songCounter += step*11;
     
     // a color in this BGR encoding is BBBB BGGG GGGR RRRR
     unsigned int color = (blue << 11) | (green << 5) | red;
-    if(step < 10) {
-      buzzer_set_period(1000);
-    }
-    if(step > 30) {
-      buzzer_init();
-    }
-    fillRectangle(15, 40+nameCounter, 10, 10, color);  //middle of T
-    fillRectangle(40, 40+nameCounter, 10, 10, color);  //left side of o
-    fillRectangle(60, 40+nameCounter, 10, 10, color);  //right side of o
-    fillRectangle(5+nameCounter, 40, 10, 10, color);   //top of T 
-    fillRectangle(40+nameCounter, 60, 10, 10, color);  //top of o 
-    fillRectangle(40+nameCounter, 40, 10, 10, color);  //bottom of o
-    fillRectangle(75, 40+nameCounter, 10, 10, color);  //left m column
-    fillRectangle(95, 40+nameCounter, 10, 10, color); //midle m column
-    fillRectangle(115, 40+nameCounter, 10, 10, color); //right m column
-    fillRectangle(75+nameCounter, 40, 10, 10, color);  //top left row of m
-    fillRectangle(95+nameCounter, 40, 10, 10, color); //top right row of m
+    fillRectangle(15, 15+nameCounter, 10, 10, color);  //middle of T
+    fillRectangle(40, 15+nameCounter, 10, 10, color);  //left side of o
+    fillRectangle(60, 15+nameCounter, 10, 10, color);  //right side of o
+    fillRectangle(5+nameCounter, 15, 10, 10, color);   //top of T 
+    fillRectangle(40+nameCounter, 35, 10, 10, color);  //top of o 
+    fillRectangle(40+nameCounter, 15, 10, 10, color);  //bottom of o
+    fillRectangle(75, 15+nameCounter, 10, 10, color);  //left m column
+    fillRectangle(95, 15+nameCounter, 10, 10, color); //midle m column
+    fillRectangle(115, 15+nameCounter, 10, 10, color); //right m column
+    fillRectangle(75+nameCounter, 15, 10, 10, color);  //top left row of m
+    fillRectangle(95+nameCounter, 15, 10, 10, color); //top right row of m
     
-    fillRectangle(10 + step, 80, 5 + step, 5, color); // line under name
+    fillRectangle(10 + step, 50, 5 + step, 10, color); // line under name
     nameCounter++;
     if (nameCounter >= 20){
       nameCounter = 0;
     }
     step++;
     if (switch1_down) {         //CAP A
-      // buzzer_init();
-      // buzzer_set_period(1000);
+      buzzer_set_period(songCounter);
       green = (green + 3) % 37;
-      fillRectangle(0 + (step)*(2), 90, 40 + (step)*(2), 20 + step, color);
+      currentX = 0 + (step)*(2);
+      currentY = 70;
+      currentWidth = (step)*(2);
+      currentHeight = 20 + step;
+      fillRectangle(currentX, currentY, currentWidth, currentHeight, color);
+      
       if (switch2_down) {            //CAP B
-	 step = 0;
-	clearScreen(COLOR_YELLOW);
-	drawString5x7(20, 10, "This is YELLOW", COLOR_GREEN, COLOR_YELLOW);
-	if (switch3_down){           //CAP C
-	   step = 0;
-	  clearScreen(COLOR_RED);
-	  drawString5x7(20, 10, "This is RED", COLOR_GREEN, COLOR_RED);
-	}
+	
+	drawString5x7(5, 75, "RADICAL", COLOR_GREEN, color);
+        
       }
       else if (switch3_down) {       //CAP B
-	 step = 0;
-	clearScreen(COLOR_GREEN);
-	drawString5x7(20, 10, "This is GREEN", COLOR_RED, COLOR_GREEN);
+	step = 0; 
+	drawString5x7(20, 100, "GROOVY", COLOR_BLUE, color);
       }
     }
     else if (switch2_down) {         //CAP A
-      // buzzer_set_period(1500);
+      buzzer_set_period(songCounter);
       blue = (blue + 2) % 27;
-      if(step <= 60){
-	fillRectangle(step, 150 - step, 1 + step , 1, color);
+      currentX = step;
+      currentY = 150 - step;
+      fillRectangle(currentX, currentY, currentWidth, currentHeight, color);
+      
+      if(switch3_down) {             //CAP B 
+	drawString5x7(5, 140, "NICE", COLOR_RED, color);
       }
-      if(switch3_down) {             //CAP B
-	clearScreen(COLOR_BLUE);
-	 step = 0;
-	drawString5x7(20, 10, "This is BLUE", COLOR_RED, COLOR_BLUE);
+      if(switch4_down) {
+	drawString5x7(50, 120, "NEATO REANO", COLOR_RED, color);
       }
     }
     else if(switch3_down) {          //CAP A
       red = (red - 3) % 17;
-      // buzzer_set_period(2000);
-      if(step <= 60){
-	fillRectangle(60, 150 - step, 1 + step, 1, color);
+      buzzer_set_period(songCounter);
+      if(switch4_down){
+	drawString5x7(75, 0, "SURPRISE!!!", COLOR_PURPLE, color);
       } 
     }
     else if(switch4_down) {          //CAP A
-      // buzzer_set_period(3000);
-      if(step == 0 ){
-	fillRectangle(9 + nameCounter, 9, 60, 20, COLOR_BLACK);
-	fillRectangle(60, 9, 60, 20, COLOR_BLACK);
-      }
-      if(step >= 20){
-	fillRectangle(60 + nameCounter, 9, 1, 20, COLOR_BLACK);
-      }
-      fillRectangle(5 + nameCounter, 5, 60, 20, COLOR_BLACK);
-      drawString5x7(10 + nameCounter, 10, "SUCCESS!!!", COLOR_GREEN, COLOR_BLACK);
+      buzzer_set_period(songCounter);
+      clearScreen(color);
     }
   }else {
     step = 0;
+    bounce2 = 0;
   }
 }
 
